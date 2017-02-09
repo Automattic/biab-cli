a249a86b6f1294a6d5165f0916a120d79dde7fdf
John Godley <johng75@gmail.com>
2017-02-09 13:01:03 +0000

    Sensehat CLI to capture data

---
+++ sensehat/capture.js
@@ -0,0 +1,21 @@
+const exec = require( 'child_process' ).exec;
+const debug = require( 'debug' )( 'biab:sensehat:capture' );
+
+function capture( wp, commandData ) {
+	const cmd = `/usr/bin/python ${ __dirname }/capture.py`;
+
+	debug( 'Capturing data: ' + cmd );
+
+	exec( cmd, ( error, stdout, stderr ) => {
+		if ( error ) {
+			debug( 'Unable to capture sensehat data' );
+			this.emit( 'error', 'Unable to capture sensehat data' );
+		} else {
+			const json = JSON.parse( stdout );
+			debug( 'Data captured - temp=' + json.temperature + ' humidity=' + json.humidity + ' pressure=' + json.pressure );
+			this.emit( 'sensehat-reading', wp, commandData, json );
+		}
+	} );
+}
+
+module.exports = capture;
---
+++ sensehat/capture.py
@@ -0,0 +1,14 @@
+import json
+from sense_hat import SenseHat
+
+def main():
+	sense = SenseHat()
+	tem_pressure = sense.get_temperature_from_pressure()
+	tem_humidity = sense.get_temperature_from_humidity()
+	tem = (tem_humidity + tem_pressure) / 2
+	hum = sense.get_humidity()
+	air = sense.get_pressure()
+	print json.dumps( { 'temperature': tem, 'humidity': hum, 'pressure': air } )
+
+if __name__ == "__main__":
+	main()
