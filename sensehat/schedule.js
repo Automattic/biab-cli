cbacb3567ee3e92ec425a9bfa32c14c350381408
John Godley <johng75@gmail.com>
2017-02-09 13:00:40 +0000

    Allow sensehat to schedule itself

---
+++ sensehat/schedule.js
@@ -0,0 +1,16 @@
+const path = require( 'path' );
+const cron = require( '../cron' );
+
+function setCameraCron( wp, schedule ) {
+	const cmd = path.resolve( path.join( __dirname, '..', 'biab' ) ) + ' sensehat-capture';
+	const success = () => {
+		this.emit( 'result', 'scheduled' );
+	};
+	const error = msg => {
+		this.emit( 'error', msg );
+	};
+
+	cron( cmd, wp, schedule, success, error );
+}
+
+module.exports = setCameraCron;
