1496b011b870f06259ed7c70ba231e2af3f75212
John Godley <johng75@gmail.com>
2017-02-10 09:16:07 +0000

    Add unified config system
    Rather than each device manage it’s own config we have a central config that everyone can use

---
+++ config/index.js
@@ -0,0 +1,42 @@
+const debug = require( 'debug' )( 'biab:config' );
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+const filename = () => path.resolve( path.join( __dirname, 'config.json' ) );
+
+function setConfig( section, value ) {
+	const config = getConfig();
+
+	debug( 'Writing to config: ' + section, value );
+	config[section] = value;
+
+	fs.writeFileSync( filename(), JSON.stringify( config ) );
+}
+
+function getConfig( section, defaults ) {
+	let config;
+
+	try {
+		config = JSON.parse( fs.readFileSync( filename() ) );
+	} catch ( e ) {
+		debug( 'No config, creating empty' );
+		config = {};
+	}
+
+	if ( section ) {
+		if ( config[section] ) {
+			debug( 'Section ' + section, config[section] );
+			return config[section];
+		}
+
+		debug( 'Section ' + section + ' doesnt exist - using defaults', defaults );
+		return defaults;
+	}
+
+	return config;
+}
+
+module.exports = {
+	set: setConfig,
+	get: getConfig,
+};
